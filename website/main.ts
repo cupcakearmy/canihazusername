12f7c4594f689b680abe93d9953b3613e8260773
cupcakearmy <hi@nicco.io>
2020-02-05 22:48:14 +0100

    website

--- website/main.ts
+++ website/main.ts
@@ -1,3 +1,17 @@
-import cihu from '../'
+import 'bulma/css/bulma'
+import { showAvailableLists, generate } from '../'
 
-console.log(cihu())
+const input = window.document.getElementById('format') as HTMLInputElement
+const output = window.document.getElementById('username') as HTMLInputElement
+const list = window.document.getElementById('lists')
+const button = window.document.getElementById('button')
+
+const gen = () => {
+    output.value = generate(input.value)
+}
+
+button.addEventListener('click', gen)
+button.addEventListener('touchstart', gen)
+
+gen()
+list.innerHTML = showAvailableLists().join(', ')
